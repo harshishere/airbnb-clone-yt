195047cf2e720606952b31171b58308127b6aad1
Harsh Bairagi <harsh1bairagi@gmail.com>
2021-10-11 17:29:44 +0530

    first commit

---
+++ components/icons/SearchIcon.js
@@ -0,0 +1,22 @@
+function SearchIcon() {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export default SearchIcon;
